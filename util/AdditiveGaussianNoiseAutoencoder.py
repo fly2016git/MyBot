bea4c81f3e36401812c6b960717f44b8e505754a
zpf_s <zpf_s@DESKTOP-L2STAU4>
2017-04-14 07:54:48 +0800

    测试去噪编码器

--- util/AdditiveGaussianNoiseAutoencoder.py
+++ util/AdditiveGaussianNoiseAutoencoder.py
@@ -5,73 +5,7 @@ Created on 2017年4月12日
 @author: 206
 '''
 
-import numpy as np
-import sklearn.preprocessing as prep
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
-
-def xavier_init(fan_in, fan_out, constant = 1):
-    '''
-    一种参数初始化方法xavier initialization，可以根据输入、输出节点的数量调整最合适的分布，让权重初始化的不大不小，正好合适。
-    fan_in:输入节点的数量
-    fan_out：输出节点的数量
-    '''
-    low = -constant * np.sqrt(6.0/(fan_in + fan_out))
-    high = constant * np.sqrt(6.0/(fan_in + fan_out))
-    return tf.random_uniform((fan_in, fan_out), minval = low, maxval = high, dtype = tf.float32)
-
-
-def standard_scale(X_train, X_test):
-    '''
-    对训练数据和测试数据进行标准化处理
-    '''
-    preprocessor = prep.StandardScaler().fit(X_train)
-    X_train = preprocessor.transform(X_train)
-    X_test = preprocessor.transform(X_test)
-    return X_train, X_test
-
-def get_random_block_from_data(data, batch_size):
-    '''
-    随机获取数据块
-    '''
-    start_index = np.random.randint(0, len(data) - batch_size)
-    return data[start_index:(start_index + batch_size)]
-
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True) 
-
-def run(X_train=mnist.train.images, X_test=mnist.test.images):
-    # 标准化变换
-    X_train, X_test = standard_scale(X_train, X_test)
-    # 定义参数：总训练样本数、最大训练轮数、batch_size , 并设置每隔一轮显示一次cost
-    n_samples = int(mnist.train.num_examples)
-    training_epochs = 20
-    batch_size = 128
-    display_step = 1
-    
-    # 创建自编码器实例
-    autoencoder = AdditiveGaussianNoiseAutoencoder(
-        n_input=784,
-        n_hidden = 200,
-        transfer_function=tf.nn.softplus,
-        optimizer= tf.train.AdamOptimizer(learning_rate=0.001),
-        scale=0.01                                 )
-    
-    
-    for epoch in range(training_epochs):
-        avg_cost = 0
-        total_batch = int(n_samples/batch_size)
-        for i in range(total_batch):
-            batch_xs = get_random_block_from_data(X_train, batch_size)
-            
-            cost = autoencoder.partial_fit(batch_xs)
-            avg_cost += cost / n_samples*batch_size
-            
-        if epoch%display_step == 0:
-            print('Epoch:', '%04d' % (epoch+1), 'cost=', "{:.9f}".format(avg_cost))
-            
-    print("total cost: " + str(autoencoder.calc_total_cost(X_test)))
-            
 
 class AdditiveGaussianNoiseAutoencoder(object):
     '''
@@ -119,6 +53,7 @@ class AdditiveGaussianNoiseAutoencoder(object):
                                   feed_dict={
                                       self.x:X,
                                       self.scale:self.training_scale})
+        return cost
     
     # 计算损失值    
     def calc_total_cost(self, X):
