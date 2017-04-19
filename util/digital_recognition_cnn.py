14a78972e2fd8a35c0a63dc2fc7e6b2cde4da632
zpf_fly2016 <fly2015_zhang@163.com>
2017-04-19 17:18:54 +0800

    卷积神经网络识别手写数字

--- util/digital_recognition_cnn.py
+++ util/digital_recognition_cnn.py
@@ -26,10 +26,12 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 # 卷积采用1步长，0边距，保证输入输出大小相同
+# padding='SAME',表示边界补充0,以保证输入输出size相同
+# padding='CALID',不补充
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-# 池化采用2×2模板
+# 池化采用2×2模板，最大池化函数
 def max_pool_2x2(x):
     return  tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
@@ -38,12 +40,12 @@ x = tf.placeholder(tf.float32, [None, 784])
 # 输出类别共10个：0-9
 y_ = tf.placeholder('float', [None, 10])
 
-# 第一层卷积权重，视野是5*5，输入通道1个，输出通道32个
+# 第一层卷积权重，视野是5*5，输入通道1个; 输出通道32个,表示32个卷积核
 W_conv1 = weight_variable([5, 5, 1, 32])
 # 第一层卷积偏置项有32个
 b_conv1 = bias_variable([32])
 
-# 把x变成4d向量，第二维和第三维是图像尺寸，第四维是颜色通道数1
+# 把x变成2d向量，第一维-1表示样本数量不固定，第二维和第三维是图像尺寸，第四维是颜色通道数，此处我们使用的图像只有灰度单色，所以是1
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
@@ -58,7 +60,7 @@ b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-# 第二层池化后尺寸编程7*7，第三层是全连接，输入是64个通道，输出是1024个神经元
+# 第二层池化后尺寸变成7*7，第三层是全连接，输入是64个通道，输出是1024个神经元
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 # 第三层全连接偏置项有1024个
 b_fc1 = bias_variable([1024])
@@ -94,6 +96,8 @@ for i in range(20000):
         print("step %d, training accuracy %g"%(i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
+# 输出识别结果
+print(sess.run(tf.argmax(y_conv, 1), feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
