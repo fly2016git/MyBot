ea7e1a3133080a11e073e967f3c5ea4f57b69aa5
zpf_s <zpf_s@DESKTOP-L2STAU4>
2017-05-03 00:28:10 +0800

    词向量

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ Created on 2017年4月9日
 
 @author: zpf_s
 '''
-print('hello')
+ls = [1,2,3,4,5,6]
+for _ in range(5):
+    print(_)
---
+++ util/cifar10_cnn.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+'''
+Created on 2017年4月20日
+
+@author: zpf_s
+'''
+import cifar10, cifar10_input
+import tensorflow as tf
+import numpy as np
+import time
